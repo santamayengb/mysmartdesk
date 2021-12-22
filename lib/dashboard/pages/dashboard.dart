b0192aec3e90bac3950178449f057be1a879c8cd
santamayengb <84440714+santamayengb@users.noreply.github.com>
2021-12-22 20:41:31 +0530

    added bottom nav

--- lib/dashboard/pages/dashboard.dart
+++ lib/dashboard/pages/dashboard.dart
@@ -1,78 +1,108 @@
+// import 'package:flutter/material.dart';
+// import 'package:mysmartdesk/authentication/data/constant/kcolor.dart';
+// import 'package:mysmartdesk/authentication/logic/authflow/authflow_cubit.dart';
+
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:mysmartdesk/dashboard/data/model/card.model.dart';
+
+// import 'package:mysmartdesk/dashboard/logic/card/card_cubit.dart';
+// import 'package:mysmartdesk/dashboard/widgets/medname_widget.dart';
+
+// class DashboardPage extends StatelessWidget {
+//   DashboardPage({Key? key}) : super(key: key);
+
+//   final nameController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final state = context.watch<CardCubit>().state;
+//     final data = state.card;
+
+//     return Scaffold(
+//       backgroundColor: Colors.black,
+//       appBar: AppBar(
+//         backgroundColor: Colors.transparent,
+//         title: const Text("Dashboard"),
+//         actions: [
+//           IconButton(
+//               onPressed: () => context.read<AuthflowCubit>().logout(),
+//               icon: const Icon(Icons.person))
+//         ],
+//       ),
+//       body: ListView.builder(
+//         itemCount: data.length,
+//         itemBuilder: (context, index) {
+//           final d = data[index];
+
+//           return ListTile(
+//             title: Text(d.medName),
+//             subtitle: Text(d.time),
+//             onLongPress: () => context.read<CardCubit>().delete(d),
+//           );
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         backgroundColor: kPrimaryColor,
+//         onPressed: () {
+//           showModalBottomSheet(
+//               isScrollControlled: true,
+//               context: context,
+//               builder: (context) {
+//                 return Padding(
+//                   padding: EdgeInsets.only(
+//                       bottom: MediaQuery.of(context).viewInsets.bottom),
+//                   child: Container(
+//                     height: 250,
+//                     color: kPrimaryColor,
+//                     child: Column(
+//                       children: [
+//                         MedNameWidget(
+//                           nameController: nameController,
+//                         ),
+//                         TextButton(
+//                             onPressed: () {
+//                               context.read<CardCubit>().add(CardModel(
+//                                   medName: nameController.text, time: "time"));
+//                             },
+//                             child: const Text("Okay"))
+//                       ],
+//                     ),
+//                   ),
+//                 );
+//               });
+//         },
+//         child: const Icon(Icons.add),
+//       ),
+//     );
+//   }
+// }
 import 'package:flutter/material.dart';
-import 'package:mysmartdesk/authentication/data/constant/kcolor.dart';
-import 'package:mysmartdesk/authentication/logic/authflow/authflow_cubit.dart';
 
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mysmartdesk/dashboard/data/model/card.model.dart';
-
-import 'package:mysmartdesk/dashboard/logic/card/card_cubit.dart';
-import 'package:mysmartdesk/dashboard/widgets/medname_widget.dart';
+import 'package:mysmartdesk/router/router.dart';
 
 class DashboardPage extends StatelessWidget {
-  DashboardPage({Key? key}) : super(key: key);
-
-  final nameController = TextEditingController();
+  const DashboardPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final state = context.watch<CardCubit>().state;
-    final data = state.card;
-
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        title: const Text("Dashboard"),
-        actions: [
-          IconButton(
-              onPressed: () => context.read<AuthflowCubit>().logout(),
-              icon: const Icon(Icons.person))
-        ],
-      ),
-      body: ListView.builder(
-        itemCount: data.length,
-        itemBuilder: (context, index) {
-          final d = data[index];
-
-          return ListTile(
-            title: Text(d.medName),
-            subtitle: Text(d.time),
-            onLongPress: () => context.read<CardCubit>().delete(d),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: kPrimaryColor,
-        onPressed: () {
-          showModalBottomSheet(
-              isScrollControlled: true,
-              context: context,
-              builder: (context) {
-                return Padding(
-                  padding: EdgeInsets.only(
-                      bottom: MediaQuery.of(context).viewInsets.bottom),
-                  child: Container(
-                    height: 250,
-                    color: kPrimaryColor,
-                    child: Column(
-                      children: [
-                        MedNameWidget(
-                          nameController: nameController,
-                        ),
-                        TextButton(
-                            onPressed: () {
-                              context.read<CardCubit>().add(CardModel(
-                                  medName: nameController.text, time: "time"));
-                            },
-                            child: const Text("Okay"))
-                      ],
-                    ),
-                  ),
-                );
-              });
-        },
-        child: const Icon(Icons.add),
-      ),
+    return AutoTabsScaffold(
+      routes: const [AdminDashboardRoute(), UserDashboardRoute()],
+      bottomNavigationBuilder: (context, tabsRouter) {
+        return BottomNavigationBar(
+          currentIndex: tabsRouter.activeIndex,
+          onTap: tabsRouter.setActiveIndex,
+          items: const [
+            BottomNavigationBarItem(
+              label: "Medicine",
+              icon: Icon(Icons.medical_services_sharp),
+            ),
+            BottomNavigationBarItem(
+              label: "Dashboard",
+              icon: Icon(Icons.dashboard),
+            )
+          ],
+        );
+      },
     );
   }
 }
